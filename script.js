f6f6fb9237fdf36d283832f89adc633ce9341312
Henryk Martusewicz <57048118+perz1val-git@users.noreply.github.com>
2021-07-01 12:24:12 +0200

    stuff

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apikey = "5b3ce3597851110001cf62485d3b3c576a73420595830fb7bb9c1908";
+
 var map = L.map("mapid").setView([52.2, 21.0], 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -5,4 +7,11 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   minZoom: 3,
   maxZoom: 19,
+}).addTo(map);
+
+L.Routing.control({
+  router: new L.Routing.openrouteservice(
+    "5b3ce3597851110001cf62485d3b3c576a73420595830fb7bb9c1908"
+  ),
+  waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
 }).addTo(map);
